a8c7da7166712bd804832352f1a9007ca63f8c58
khalvai <khalvayozbek@gmail.com>
2023-08-03 10:16:36 +0330

    fix : test  config

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,24 +1,83 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "src/app.module";
 
-describe('AppController (e2e)', () => {
+describe("Auth", () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe("signup", () => {
+    const signupMutation = `mutation Signup($signupInput: SignupInput!) {
+      signup(SignupInput: $signupInput) {
+        token
+        name
+      }
+    }`;
+
+  
+
+    it("should give validation error", async () => {
+      const variables = {
+        signupInput: {
+          name: "john",
+          email: "test",
+          confirmPassword: "wrongPassword",
+          password: "test",
+        },
+      };
+
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .send({
+          query: signupMutation,
+          variables,
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.errors[0].message).toContain(
+        "email must be an email"
+      );
+
+      expect(response.body.errors[0].message).toContain(
+        "Passwords do not match!"
+      );
+    });
+
+    it("should give user already exists", async () => {
+      const variables = {
+        signupInput: {
+          name: "john",
+          email: "test@gmail.com",
+          confirmPassword: "test",
+          password: "test",
+        },
+      };
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .send({
+          query: signupMutation,
+          variables,
+        });
+      console.log(response.body.errors);
+
+      expect(response.status).toBe(200);
+      expect(response.body.errors[0].message).toContain(
+        "user with this email already exists, please try to login"
+      );
+    });
   });
 });
