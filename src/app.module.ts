6ecb2a790d0c1f25ebc5148553f6ff064706b942
khalvai <khalvayozbek@gmail.com>
2023-08-03 07:55:05 +0330

    setup : project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "path";
+import { GraphQLError, GraphQLFormattedError } from "graphql";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get("DATABASE_URL"),
+      }),
+      inject: [ConfigService],
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      driver: ApolloDriver,
+      formatError: (error: GraphQLError) => {
+        const graphQLFormattedError: GraphQLFormattedError = {
+          message:
+            (error?.extensions.originalError as any)?.message ||
+            (error?.extensions?.exception as any)?.response?.message ||
+            error?.message,
+        };
+
+        return graphQLFormattedError;
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
