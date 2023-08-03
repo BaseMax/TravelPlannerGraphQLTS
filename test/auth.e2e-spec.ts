2567d506b0151fd6fe1d98ee5f21d356c00045aa
khalvai <khalvayozbek@gmail.com>
2023-08-03 11:36:06 +0330

    add  test : user sign up

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,9 +2,14 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "src/app.module";
-
+import { Model } from "mongoose";
+import { getModelToken } from "@nestjs/mongoose";
+import { getFakeUser } from "./fakeData/user.fake";
+import * as argon2 from "argon2";
 describe("Auth", () => {
   let app: INestApplication;
+  let userModel: Model<any>;
+  const fakeUser = getFakeUser();
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,6 +18,13 @@ describe("Auth", () => {
 
     app = moduleFixture.createNestApplication();
     app.useGlobalPipes(new ValidationPipe());
+    userModel = app.get<Model<any>>(getModelToken("User"));
+
+    await userModel.deleteMany();
+    await userModel.create({
+      ...fakeUser,
+      password: await argon2.hash(fakeUser.password),
+    });
     await app.init();
   });
 
@@ -27,8 +39,6 @@ describe("Auth", () => {
         name
       }
     }`;
-
-  
 
     it("should give validation error", async () => {
       const variables = {
@@ -57,11 +67,35 @@ describe("Auth", () => {
       );
     });
 
-    it("should give user already exists", async () => {
+    it("should give already exists user error", async () => {
       const variables = {
         signupInput: {
           name: "john",
           email: "test@gmail.com",
+          confirmPassword: "test",
+          password: "test",
+        },
+      };
+
+      const response = await request(app.getHttpServer())
+        .post("/graphql")
+        .send({
+          query: signupMutation,
+          variables,
+        });
+      console.log(response.body.errors);
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeNull();
+      expect(response.body.errors[0].message).toBe(
+        "user with this email exists, please try to login"
+      );
+    });
+    it("should signup successfully", async () => {
+      const variables = {
+        signupInput: {
+          name: "john",
+          email: "test2@gmail.com",
           confirmPassword: "test",
           password: "test",
         },
@@ -72,12 +106,9 @@ describe("Auth", () => {
           query: signupMutation,
           variables,
         });
-      console.log(response.body.errors);
 
       expect(response.status).toBe(200);
-      expect(response.body.errors[0].message).toContain(
-        "user with this email already exists, please try to login"
-      );
+      expect(response.body.data.signup).toBeTruthy();
     });
   });
 });
