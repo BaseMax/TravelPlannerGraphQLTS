42e6d66c7f53f1ad01cdfb341838c06a40691996
khalvai <khalvayozbek@gmail.com>
2023-08-07 09:28:51 +0330

    add : multiple fake users

--- test/fakeData/user.fake.ts
+++ test/fakeData/user.fake.ts
@@ -1,10 +1,18 @@
 import { User } from "src/user/entities/user.entity";
 
-export function getFakeUser(): User {
-  return {
-    name: "john",
-    email: "test@gmail.com",
-    password: "test",
-    createdAt: new Date(),
-  };
+export function getFakeUsers(): User[] {
+  return [
+    {
+      name: "john",
+      email: "test@gmail.com",
+      password: "test",
+      createdAt: new Date(),
+    },
+    {
+      name: "john",
+      email: "test2@gmail.com",
+      password: "test",
+      createdAt: new Date(),
+    },
+  ];
 }
