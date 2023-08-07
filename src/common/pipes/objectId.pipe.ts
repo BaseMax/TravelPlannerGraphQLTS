adec9830d1f1a5bd7f6391263e07a9abb5cfe72c
khalvai <khalvayozbek@gmail.com>
2023-08-07 08:12:09 +0330

    hot fix : objectId pipe

--- src/common/pipes/objectId.pipe.ts
+++ src/common/pipes/objectId.pipe.ts
@@ -3,16 +3,27 @@ import {
   BadRequestException,
   PipeTransform,
 } from "@nestjs/common";
-import { isValidObjectId } from "mongoose";
-
+import mongoose from "mongoose";
 export class ParseObjectIdPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    const isValidId = isValidObjectId(value);
+    const isValidId = this.isValidObjectId(value);
 
     if (!isValidId) {
       throw new BadRequestException("id must be a valid objectId");
     }
 
     return value;
+  }
+
+  isValidObjectId(id: string): boolean {
+    try {
+      if (mongoose.Types.ObjectId.isValid(id)) {
+        if (String(new mongoose.Types.ObjectId(id)) === id) return true;
+        return false;
+      }
+      return false;
+    } catch {
+      return false;
+    }
   }
 }
